b2dd9b15b5f9c6b0593593efabb2706da97f6daf
Naeemkhan33 <ranker333@gmail.com>
2021-01-03 19:57:41 +0500

    added checkout-bar

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
+import { faCartPlus, faIgloo } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+
+// Images
+import EmptyBox from '../../assets/images-svg/empty.svg';
 
 const cardArr = [
   {
@@ -139,9 +142,80 @@ const cardArr = [
   },
 ];
 
+const checkoutItem = [
+  {
+    image:
+      'https://s3.amazonaws.com/redqteam.com/pickbazar/Roadstar_solid_top.jpg',
+    name: 'Roadster Women Solid Top',
+    stock: '1 pc(s)',
+    minusPrice: '$15',
+    price: '$12',
+    discount: '20%',
+  },
+];
+
 export const ShopPage = () => {
   return (
     <section>
+      {/* Checkout Bar Start */}
+      <div className='checkout-bar'>
+        <header className='d-flex align-items-baseline justify-content-between pt-2 pb-3 mb-3 border-bottom px-3'>
+          <div>1 item</div>
+          <Link className='text-decoration-none'>
+            <div className='h4 mb-0'>&times;</div>
+          </Link>
+        </header>
+        <main className='px-3'>
+          {/* == */}
+          <div className='mb-3 d-flex flex-column align-items-center'>
+            <span className='d-flex' style={{ height: 150, width: 150 }}>
+              <img src={EmptyBox} height='100%' width='100%' alt='Empty' />
+            </span>
+            <small className='text-black-50 mt-3'>No products found</small>
+          </div>
+          {/* End */}
+          {/* Item */}
+          {checkoutItem.map((value) => (
+            <div className='mb-3 d-flex justify-content-between small'>
+              <span className='mr-2'>1</span>
+              <span className='mr-2 text-black-50 small'>
+                <img
+                  height={50}
+                  width={50}
+                  src={value.image}
+                  alt={value.name}
+                />
+              </span>
+              <span className='mr-2 small'>
+                <div className='text-capitalize'>{value.name}</div>
+                <div className='fw-bold'>{value.price}</div>
+                <div className='text-black-50'>{value.stock}</div>
+              </span>
+              <span>{value.price}</span>
+            </div>
+          ))}
+          {/* Item-End */}
+        </main>
+        <footer className='mt-auto px-3 py-3'>
+          <div className='mb-3'>
+            <h6>Do you have a voucher?</h6>
+            <div className='row mx-0'>
+              <div className='col-9 px-0'>
+                <input className='form-control' type='text' />
+              </div>
+              <div className='col-3'>
+                <button className='btn btn-dark'>Apply</button>
+              </div>
+            </div>
+          </div>
+          <div>
+            <button type='button' className='btn btn-dark w-100'>
+              Checkout
+            </button>
+          </div>
+        </footer>
+      </div>
+      {/* Checkout Bar End */}
       <div className='py-5 my-5 text-center'>
         <h1>
           <i>Shop</i>
