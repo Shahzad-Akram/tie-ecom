33cd39bc062e92596351b818c245af338e1da88f
Naeemkhan33 <ranker333@gmail.com>
2021-02-13 03:19:25 +0500

    styles order

--- src/components/tabs/TabOne.jsx
+++ src/components/tabs/TabOne.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import DayJS from "react-dayjs";
+import React from 'react';
+import DayJS from 'react-dayjs';
 
 const cardTabArr = [
   {
     key: 1,
-    progressText: "Order On the way",
-    date: "7th April 2019",
-    time: "17th April",
-    price: "$230",
+    progressText: 'Order On the way',
+    date: '7th April 2019',
+    time: '17th April',
+    price: '$230',
   },
 ];
 
@@ -16,31 +16,34 @@ export const TabOne = ({ onClick, data }) => {
     <>
       <div onClick={onClick}>
         <button
-          type="button"
-          className="btn w-100 bg-light p-3 small rounded-3 mb-4"
+          type='button'
+          className='btn w-100 bg-light p-3 small rounded-3 mb-4'
         >
-          <div className="d-flex justify-content-between mb-3">
-            <span className="fw-bold">Order#{data._id}</span>
-            <span className="text-capitalize badge bg-dark">
+          <div
+            className='d-flex justify-content-between mb-3'
+            style={{ lineBreak: 'anywhere' }}
+          >
+            <span className='fw-bold'>Order#{data._id}</span>
+            <span className='text-capitalize badge bg-dark'>
               {cardTabArr.progressText}
             </span>
           </div>
 
-          <div className="d-flex justify-content-between mb-2 small">
-            <span className="text-capitalize">Order Date:</span>
-            <span className="text-capitalize">
-              <DayJS format="MM-DD-YYYY">{data.createdAt}</DayJS>
+          <div className='d-flex justify-content-between mb-2 small'>
+            <span className='text-capitalize'>Order Date:</span>
+            <span className='text-capitalize'>
+              <DayJS format='MM-DD-YYYY'>{data.createdAt}</DayJS>
             </span>
           </div>
 
-          <div className="d-flex justify-content-between mb-2 small">
-            <span className="text-capitalize">Delivery Time:</span>
-            <span className="text-capitalize">1234</span>
+          <div className='d-flex justify-content-between mb-2 small'>
+            <span className='text-capitalize'>Delivery Time:</span>
+            <span className='text-capitalize'>1234</span>
           </div>
 
-          <div className="d-flex justify-content-between small fw-bold">
-            <span className="text-capitalize">Total Price:</span>
-            <span className="text-capitalize">${data.total}</span>
+          <div className='d-flex justify-content-between small fw-bold'>
+            <span className='text-capitalize'>Total Price:</span>
+            <span className='text-capitalize'>${data.total}</span>
           </div>
         </button>
       </div>
